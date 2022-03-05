0ad588fdf47a4a1dd601e3bbadc04281639cfba2
Unggul <indosaputra@gmail.com>
2022-03-05 14:25:39 +0700

    safe area design

--- lib/main.dart
+++ lib/main.dart
@@ -25,8 +25,9 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: Scaffold(
-        body: Text("Flutter Pertama"),
-      ),
+          body: SafeArea(
+        child: Text("Flutter Pertama"),
+      )),
     );
   }
 }
